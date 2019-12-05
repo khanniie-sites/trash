deface6a43989d3385988ac0028c762632710ded
Connie Ye <constanceye1316@gmail.com>
2019-12-05 16:23:18 -0500

    test

--- script.js
+++ script.js
@@ -6,3 +6,13 @@
 // checkbox for gloves
 // share toggle
 //create
+
+document
+  .querySelector('input[type="file"]')
+  .addEventListener("change", function() {
+    if (this.files && this.files[0]) {
+      var img = document.getElementById("trash-image");// $('img')[0]
+      img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+      //img.onload = imageIsLoaded;
+    }
+  });
